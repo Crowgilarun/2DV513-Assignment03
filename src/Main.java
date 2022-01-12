d03d62f48d4989283454f25a4bd3eb15fc3e6607
aj224ar <aj224ar@student.lnu.se>
2022-01-12 20:14:59 +0100

    Source code for 2DV513 Assignment 3 - Final

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         Database database = new Database("jdbc:mysql://localhost:3306/", "root", "root");
-//        For first time running, uncomment this line to set up the database
-        setUpDatabase(database);
+        //For first time running, uncomment this line to set up the database
+        //setUpDatabase(database);
         Console view = new Console();
         Controller controller = new Controller();
         controller.runProgram(view, database);
